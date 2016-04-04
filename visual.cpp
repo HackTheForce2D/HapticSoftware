d993c30db65bc937454a51f5b400fae1d854f5af
HackTheForce2D <yoliveira@etu.unistra.fr>
2016-04-04 22:32:23 +0200

    Added files via upload

--- visual.cpp
+++ visual.cpp
@@ -1,6 +1,25 @@
 #include "visual.h"
 
-Visual::Visual()
+Visual::Visual(QWidget* Parent, const QPoint& Position, const QSize& Size) :
+    QSfmlCanvas(Parent, Position, Size)
 {
+
+}
+
+void Visual::OnUpdate()
+{
+    for(int i(0);i<5;i++){
+       polygon[i].position += sf::Vector2f(std::rand()%3-1,std::rand()%3-1);
+    }
+    draw(polygon);
+}
+
+void Visual::OnInit()
+{
+polygon = sf::VertexArray(sf::LinesStrip, 5);
+for(int i(0);i<5;i++)
+{
+   polygon[i].position = sf::Vector2f(50,50)+sf::Vector2f(20*i,30*i);
+}
 
 }
--- visual.h
+++ visual.h
@@ -1,19 +1,24 @@
 #ifndef VISUAL_H
 #define VISUAL_H
 
+#include<qsfmlcanvas.h>
 #include<QVector2D>
+#include<SFML/Graphics.hpp>
 #include<physics.h>
 
-class Visual
+class Visual : public QSfmlCanvas
 {
 public:
-    Visual();
+    Visual(QWidget* Parent, const QPoint& Position, const QSize& Size);
     void setPhysics(Physics physics);
     void startDisplay();
 private:
     Physics physics_;
+    sf::VertexArray polygon;
     void getObjectLocations();
     QVector2D convertCoordinates();
+    void OnInit();
+    void OnUpdate();
 };
 
 #endif // VISUAL_H
